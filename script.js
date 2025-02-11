e42fae8a37a65d04cbd6ea5ff4d25bbca8ffe6c8
Tanuja Sinha <sinhatanuja123@gmail.com>
2025-02-11 19:00:35 +0530

    Form styled

--- script.js
+++ script.js
@@ -3,10 +3,15 @@ const newPostForm = document.getElementById("new-post-form");
 function createPost(){
     newPostForm.style.display = "block";
 }
-function addPost(){
+
+newPostForm.addEventListener("submit", function(event){
+    event.preventDefault();
+    const formData = new FormData(newPostForm);
+    
+    //create a new blog post
     newPostForm.style.display = "none";
     const blogPost=document.createElement("div");
     blogPost.textContent="This is a new blog post";
     blogPost.classList.add("blog-post");
     document.body.appendChild(blogPost);
-}
+});
